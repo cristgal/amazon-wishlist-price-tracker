41cac3a99a3de8f25be419b8ddea5ab0af151888
cristgal <cristgal@bit3.me>
2020-03-11 00:20:48 +0000

    fix: actually calling the PY

--- track-wishlist.sh
+++ track-wishlist.sh
@@ -2,7 +2,7 @@
 
 tmp=$(mktemp)
 for wl in EZ5T4B4IF5UW; do
-    /usr/local/bin/amazon-wishlist-price-tracker/ $wl 2>/dev/null >$tmp
+    /usr/local/bin/amazon-wishlist-price-tracker/track-wishlist.py $wl 2>/dev/null >$tmp
     test -s $tmp && mail -s "Amazon-Wishlist: $wl" my.user.name@gmail.com <$tmp
 done
 rm -f $tmp
